272ac43a1fa74dc5c6955b409d1c87c0e666beb5
Oleg-Ivnv <oleg.ivanov@waites.net>
2026-06-27 04:09:14 +0300

    chore: test auto fetch

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -11,7 +11,7 @@ export const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.titleSection}>
-        <Typography variant="heading">Task Log4</Typography>
+        <Typography variant="heading">Task Log5</Typography>
         <Typography variant="paragraph">
           Manage your tasks and keep track of your progress.
         </Typography>
